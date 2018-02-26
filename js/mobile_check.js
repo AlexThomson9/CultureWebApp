f8488465a3b426a6e26d8c275f2ddf85a84df86c
ConnorForsyth <connorforsyth34@gmail.com>
2018-02-26 16:39:18 +0000

    Will Now Work

--- js/mobile_check.js
+++ js/mobile_check.js
@@ -1,3 +1,3 @@
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-  $(location).attr('href', '.mobile/test.html')
+  $(location).attr('href', '.mobile/mobile.html')
 }
