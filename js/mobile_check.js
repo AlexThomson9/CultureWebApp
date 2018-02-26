571e18baf6ab292d9d5e37712f0601c6358bff59
ConnorForsyth <connorforsyth34@gmail.com>
2018-02-26 16:41:00 +0000

    A stupid . in the link

--- js/mobile_check.js
+++ js/mobile_check.js
@@ -1,3 +1,3 @@
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-  $(location).attr('href', '.mobile/mobile.html')
+  $(location).attr('href', 'mobile/mobile.html')
 }
