72cec3083312956abb09908e0783724752dba527
ConnorForsyth <connorforsyth34@gmail.com>
2018-03-08 08:46:59 +0000

    Mobile friendly modal now in place

---
+++ mobile/js/media.js
@@ -0,0 +1,17 @@
+@media (max-width: 812){
+  h4{
+    font-size: 1em;
+  }
+}
+
+@media (min-width: 1100){
+  h4{
+    font-size: 2em;
+  }
+}
+
+@media (min-width: 1350){
+  h4{
+    font-size: 3em;
+  }
+}
