281c60c72a20d15ee98087f2e58b4fd9823a89df
ConnorForsyth <connorforsyth34@gmail.com>
2018-03-22 09:38:44 +0000

    Committed so there isn't a merge conflict

--- js/register.js
+++ js/register.js
@@ -3,8 +3,25 @@ $('#submitBtn').click(function () {
   //When backend is being done, the validation for checking entry will be moved into
   // it's own function which will be refered to here
 
+  //JQuery Validate library
+  $("#regForm").validate({
+  rules: {
+    username: "required",
+    password: "required",
+    email: {
+      required: true,
+      email: true
+    },
+  }
+})
   //Boolean variable for ensuring validation has been met
-  var noIssue = true;
+
+  /*
+  if (!$.trim($('#emailField').val())) {
+          //If true message displayed to the user
+           alert("Please enter your username");
+           noIssue = false;
+       }
   //Checks if anything has been entered into the username field
   if (!$.trim($('#userField').val())) {
           //If true message displayed to the user
@@ -20,11 +37,8 @@ $('#submitBtn').click(function () {
       }
 
   //If validation fails the page does not submit
-  if (){
-    $(location).attr('href', 'mobile/mobile.html')
-  }
-  else{
-      console.log("Hello Cruel World")
-  }
+  if (noIssue == false){
+    return false;
+  }*/
 
 });
