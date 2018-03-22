3dd8b4ee40821524a9c6cb8500f86c909cbc9b23
AlexThomson9 <1507262@rgu.ac.uk>
2018-03-22 10:35:58 +0000

    Updated and Commented html files and the JS files for the maps

--- js/Map.js
+++ js/Map.js
@@ -1,31 +1,40 @@
+//Set Option selected to menu var and add event listener
 var menu = document.getElementById('Continent');
 menu.addEventListener('change', getContinent);
-
+//Set the max bounds for the map so that it doesnt duplicate the world
 var maxiBounds =  L.latLngBounds(
+//Northwest coordinate
 L.latLng(-80.83930574607542, -166.5000021457672),
+//Southeast
 L.latLng(84.32308710686083, 186.4687478542328)
 );
-var map = L.map('map', {
-  minZoom: 2.6,
-  'maxBounds': maxiBounds,
+//define the map , and set the options 
+var map = L.map('map', 
+{
+    //Minimum Zoom so that they can not zoom out to far which would distort the map
+    minZoom: 2.6,
+    //Set maxbounds to the maxbounds value created above 
+    'maxBounds': maxiBounds,
+    //No zoom buttons on the map, so it looks a bit more professional
     zoomControl:false,
+    //No attributes on the map , so the leaflet information in the bottom right
     attributionControl: false
+    //Fit the maxbounds to the map 
 }).fitBounds(maxiBounds);
-	map.createPane('labels');
-	// This pane is above markers but below popups
-	map.getPane('labels').style.zIndex = 650;
-	// Layers in this pane are non-interactive and do not obscure mouse/touch events
-	map.getPane('labels').style.pointerEvents = 'none';
-	var cartodbAttribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>';
-	var positron = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
-		attribution: cartodbAttribution
-	}).addTo(map);
-
-/*	var positronLabels = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}.png', {
-		attribution: cartodbAttribution,
-		pane: 'labels'
-	}).addTo(map);*/
-      $.getJSON("map.geojson",function(data){
+//create a pane for the labels
+map.createPane('labels');
+// This pane is above markers but below popups
+map.getPane('labels').style.zIndex = 650;
+// Layers in this pane are non-interactive and do not obscure mouse/touch events
+map.getPane('labels').style.pointerEvents = 'none';
+//Get the relevent map information
+var cartodbAttribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>';
+var positron = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', 
+{
+    attribution: cartodbAttribution
+}).addTo(map);
+        //load the map informtaion
+$.getJSON("map.geojson",function(data){
     // add GeoJSON layer to the map once the file is loaded
    geojson = L.geoJson(data).addTo(map);
    geojson.setStyle({
@@ -34,70 +43,39 @@ var map = L.map('map', {
     weight: 1
     ,noWrap: true
 });
-
-
-          var layerGroup = L.geoJSON(data, {
+//Bind the popup to just display the country, it doesnt actually popup but we use the value
+var layerGroup = L.geoJSON(data, {
   onEachFeature: function (feature, layer) {
     layer.bindPopup(feature.properties.Country);
-
-
   }
 }).addTo(map);
 
-
+//Onlick for the leaflet marker
 $(document).on("click", ".leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive", function(){
+    //Get the text in the popup
     var ctry = $(".leaflet-popup-content").text();
+    //Log for testing
     console.log(ctry);
+    //Set the country tag to the country clicked
     $('#Country').text(ctry);
    // alert($('#Country').text());
+    //remove the popup for bugs 
      $(".leaflet-popup-content").remove();
+    //log for testing
     console.log('i work');
-    //openModal();
-   // var test = feature.properties.Country;
-    //console.log(test);
-
-
 });
-
-        /*  geojson.eachLayer(function (layer) {
-		layer.bindPopup(layer.feature.properties.Country + '<button type="button" id="btn">Click Me!</button>');
-
-            var popup = layer.getPopup();
-            var content = popup.getContent();
-              console.log(content);
-
-
-
     /*  $.ajax({
-
           url:"https://restcountries.eu/rest/v2/name/" + test,
           dataType:'json',
           success: function(result){
             $("#div1").html(result);
         }
-
-
-
-
-
-
       });
             */  
-
-
-
-	//});
   });
 
-      $('#btn').click(function(){
 
-       var value = $('.leaflet-popup-content').val();
-          console.log(value);
-
-
-    //Some code
-});
-
+    //Load the json for the countrries we want
     $.getJSON('world.geo.json-master/world.geo.json-master/countries.geo.json', function (geojson) { // load file
     L.geoJson(geojson, { // initialize layer with data
         style: function (feature) { // Style option
@@ -110,7 +88,7 @@ $(document).on("click", ".leaflet-marker-icon.leaflet-zoom-animated.leaflet-inte
     }).addTo(map); // Add layer to map
 });
 
-
+    //Function for setting the colours of the map
     function style(feature) {
     return {
         fillColor: '#D46A6A',
@@ -121,18 +99,21 @@ $(document).on("click", ".leaflet-marker-icon.leaflet-zoom-animated.leaflet-inte
         fillOpacity: 0.7
     };
 }
-
+    //Load the tiles for the ocean otherwise they are just grey
     var gl = L.mapboxGL({
     accessToken: '{token}',
     style: 'https://openmaptiles.github.io/osm-bright-gl-style/style-cdn.json'
 }).addTo(map);
-
+    //set the view to the middle of the map
 	map.setView({ lat: 47.040182144806664, lng: 9.667968750000002 }, 0);
 
 
-    function getContinent(){
-     var c = document.getElementById("Continent").selectedIndex;
+    function getContinent()
+{
+    //Get the selection index of the Continent from the dropdown
+    var c = document.getElementById("Continent").selectedIndex;
      //   var strUser = c.options[c.selectedIndex].value;
+    //Have to use a json because no multidimensional arrays in Javascript .......
     var jsontest =  '{ "countries" : [' +
                     '{ "C_ID":0 , "Lat1":5.499550, "Long1":-167.276413, "Lat2":83.162102, "Long2":-52.233040},' +
                     '{ "C_ID":1 , "Lat1":34.95799531086792, "Long1":-22.148437499999996, "Lat2":66.26685631430843, "Long2":41.66015625, "geo1":50.51342652633956, "geo2": 13.0078125 },' +
@@ -141,47 +122,49 @@ $(document).on("click", ".leaflet-marker-icon.leaflet-zoom-animated.leaflet-inte
                     '{ "C_ID":4 , "Lat1":5.499550, "Long1":-167.276413, "Lat2":83.162102, "Long2":-52.233040, "geo1":18.979025953255267, "geo2":97.998046875},' +
                     '{ "C_ID":5 , "Lat1":5.499550, "Long1":-167.276413, "Lat2":83.162102, "Long2":-52.233040, "geo1":4.740675384778373, "geo2":21.4453125},' +
                     '{ "C_ID":6 , "Lat1":5.499550, "Long1":-167.276413, "Lat2":83.162102, "Long2":-52.233040, "geo1":-24.926294766395582, "geo2":137.900390625}]}';
+    //..parse the variable with the information into JSON
     var obj = JSON.parse(jsontest);
       //  alert(c);
-        if(c != null)
-        {
-        //    alert(obj.countries[c].C_ID);
+    //if the index is not equal to null then continue
+    if(c != null)
+    {
+            //while the Id in the json object is equal to the object selected
             if(obj.countries[c].C_ID == c)
             {
-              var arraytest = obj.countries[c].C_ID;
-            //  alert(arraytest);
+                //set coordinates to Each value within the JSON object
                   var cord_lat1 = obj.countries[c].Lat1;
                   var cord_long1 = obj.countries[c].Long1;
                   var cord_lat2 = obj.countries[c].Lat2;
                   var cord_long2 = obj.countries[c].Long2;
               //    alert(cord_lat1 + "" + cord_long1 + "" + cord_lat2 +"" + cord_long2);
+                //Set the max Bounds for the map from the coordiantes gathered
                   var maxBounds = L.latLngBounds(
                   L.latLng(cord_lat1, cord_long1),
                   L.latLng(cord_lat2, cord_long2)
                   );
+                //Get the rough Middle of each region from the JSON
                   var cord_geo1 = obj.countries[c].geo1;
                   var cord_geo2 = obj.countries[c].geo2;
+                //Make vairable c_cords which is the centre point of the region to pan to
                   c_cords = L.latLng(
                       L.latLng(cord_geo1, cord_geo2),
                   );
+                //Log it to ensure that its functioning properly
                   console.log(c_cords);
             }
+            //Set the Zoom on the map so that it zooms before panning, otherwise it will clash with the maxbounds set before
             map.setZoom(3.55);
-
+            //Time delay to prevent/fix bug in the panning
             var delayInMilliseconds = 1000; //1 second
-
+            //Make the timeout and set the function to pan the map to the coordiates selected in the above function
             setTimeout(function()
             {
             map.panTo(c_cords);
+            //clear the c_cords after its been used to decrease the chance of bugs.
             delete c_cords;
+            //Preform the delay set before
             }, delayInMilliseconds);
-
-
         }
-      //  map.panTo(new L.LatLng( 39.639537564366684, -99.84374999999999,));
-      //  map.setZoom(8);
-       //map.setView(c_cords, 8);
-      //  map.fitBounds(maxBounds);
     }
-  //  geojson = L.geoJson({style: style}).addTo(map);
-	// geojson = L.geoJson(euCountries).addTo(map);
+
+
