6220b61ada8d56934653801f1b90a29c0e3eb953
ConnorForsyth <connorforsyth34@gmail.com>
2018-03-22 11:00:24 +0000

    Validation finished for front end

--- js/login.js
+++ js/login.js
@@ -2,29 +2,5 @@
 $('#submitBtn').click(function () {
   //When backend is being done, the validation for checking entry will be moved into
   // it's own function which will be refered to here
-
-  //Boolean variable for ensuring validation has been met
-  var noIssue = true;
-  //Checks if anything has been entered into the username field
-  if (!$.trim($('#userField').val())) {
-          //If true message displayed to the user
-           alert("Please enter your username");
-           noIssue = false;
-       }
-
-  //Checks if anything has been entered into the password field
-  if (!$.trim($('#passField').val())) {
-          //If true message displayed to the user
-           alert("Please enter your password");
-           noIssue = false;
-      }
-
-  //If validation fails the page does not submit
-  if (){
-    $(location).attr('href', 'mobile/mobile.html')
-  }
-  else{
-      console.log("Hello Cruel World")
-  }
-
+    $(location).attr('href', 'test.html')
 });
