c54a2d9b1ded5ee465624537bd0630446f1ea130
ConnorForsyth <connorforsyth34@gmail.com>
2018-03-29 10:12:54 +0100

    Attempt ????

--- js/countryModal.js
+++ js/countryModal.js
@@ -34,7 +34,7 @@ $(document).on("click", ".leaflet-marker-icon.leaflet-zoom-animated.leaflet-inte
            dataType:'json',
            success: function(result){
              //In here loop through JSON file to display info
-             $(".restName").append(result[0].capital);             
+             $(".restName").empty().append(result[0].capital);             
              console.log(result[0].capital);
          }
        });
