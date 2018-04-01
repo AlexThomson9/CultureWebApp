59f4798ec94950f3f06ec467920a1b7419157d87
AlexThomson9 <1507262@rgu.ac.uk>
2018-04-01 22:30:56 +0100

    plswork

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017/star_wars_quotes";
 const express = require('express');
 var fs = require('fs');
+var url = require('url');
 const bodyParser = require('body-parser');
 const app = express();
 app.use(express.static('public'));
