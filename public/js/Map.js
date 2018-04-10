bfe9336389334505f6d3a3490f1a29e4933dc408
AlexThomson9 <1507262@rgu.ac.uk>
2018-04-10 11:27:13 +0100

    test

--- public/js/Map.js
+++ public/js/Map.js
@@ -41,7 +41,8 @@ $.getJSON("map.geojson",function(data){
     delete data2[1]._id;
     delete data2[2]._id;
     delete data2[3]._id;
-    data2[0].geometry.coordinates[0].toString().replaceAll("\"", "");
+  //  data2[0].geometry.coordinates[0].toString().replaceAll("\"", "");
+  console.log(  data2[0].geometry.coordinates[0] + "" +   data2[0].geometry.coordinates[1]);
 
     var mapinfo = {}
     mapinfo.type = "FeatureCollection";
