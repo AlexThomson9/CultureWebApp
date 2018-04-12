075fe99e2f23f849b0a3df5e1480abdf0d860274
OLEK WOZNIAK (1407272) <wozniok96@gmail.com>
2018-04-12 09:30:40 +0100

    username check

--- public/js/register.js
+++ public/js/register.js
@@ -8,33 +8,47 @@ $('#submitBtn').click(function(){
   obj.username = $('#userField').val();
   obj.password = $('#passField').val();
 
+  User.find({ 'username': username,'email':email }, function(err, user) {
+
+        if (err) {
+
+            console.log('Signup error');
+            return done(err);
+        }
+
+        //if user found.
+        if (user.length!=0) {
+          if(user[0].username){
+            console.log('Username already exists, username: ' + username);
+             }else{
+                console.log('EMAIL already exists, email: ' + email);
+             }
+             var err = new Error();
+            err.status = 310;
+            return done(err);
+
+        }
+
   $.ajax({
         type: "POST",
         data: JSON.stringify(obj),
         contentType: "application/json",
         url: "/register"
-        });
+});
 
 
 
 
-  $('.login-box').append("<h3>Registration Successful!</h3>");
+  $('.login-box').append("<h5>Registration Successful, Now you will be redirected to the login page.</h5>");
       //set the var to 2 sectons
-          var delayInMilliseconds = 5000; //2 second
+          var delayInMilliseconds = 3000; //2 second
           //set the timeout to run the function
           setTimeout(function()
           {
           //remove the submission successful as it only needs to be there for a little bit
-          $('.login-box').find("h3").remove();
+          $('.login-box').find("h5").remove();
           window.location.replace("login.html");
           //set the timeout to use the 2 second's defined previously
         }, delayInMilliseconds);
         form.reset();
-
-
-
-
-
-
-
 });
