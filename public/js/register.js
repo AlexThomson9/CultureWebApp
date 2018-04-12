a50c061276532a25b05fda3acd94c9543f6e7eb3
OLEK WOZNIAK (1407272) <wozniok96@gmail.com>
2018-04-12 10:00:41 +0100

    bug fix 2.0

--- public/js/register.js
+++ public/js/register.js
@@ -12,24 +12,25 @@ $('#submitBtn').click(function(){
 
   User.find({ 'username': username,'email':email }, function(err, user) {
 
-        if (err) {
+      if (err) {
 
-            console.log('Signup error');
-            return done(err);
+          console.log('Signup error');
+          return done(err);
+      }
+
+      //if user found.
+      if (user.length!=0) {
+        if(user[0].username){
+          console.log('Username already exists, username: ' + username);
+           }else{
+              console.log('EMAIL already exists, email: ' + email);
+           }
+          var err = new Error();
+          err.status = 310;
+          return done(err);
+
         }
-
-        //if user found.
-        if (user.length!=0) {
-          if(user[0].username){
-            console.log('Username already exists, username: ' + username);
-             }else{
-                console.log('EMAIL already exists, email: ' + email);
-             }
-             var err = new Error();
-            err.status = 310;
-            return done(err);
-
-        });
+    });
 
   $.ajax({
         type: "POST",
