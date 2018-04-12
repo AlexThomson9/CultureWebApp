5b1f9fc7dd3e3df9572f7be610585530153188da
OLEK WOZNIAK (1407272) <wozniok96@gmail.com>
2018-04-12 10:23:22 +0100

    xd

--- public/js/register.js
+++ public/js/register.js
@@ -10,27 +10,6 @@ $('#submitBtn').click(function(){
   obj.username = $('#userField').val();
   obj.password = $('#passField').val();
 
-  db.collection('userdetails').find({ 'username': username,'email':email }, function(err, user) {
-
-      if (err) {
-
-          console.log('Signup error');
-          return done(err);
-      }
-
-      //if user found.
-      if (user.length!=0) {
-        if(user[0].username){
-          console.log('Username already exists, username: ' + username);
-           }else{
-              console.log('EMAIL already exists, email: ' + email);
-           }
-          var err = new Error();
-          err.status = 310;
-          return done(err);
-
-        }
-    });
 
   $.ajax({
         type: "POST",
--- server.js
+++ server.js
@@ -127,6 +127,27 @@ app.post('/savefile', function(req, res){
 });
 
 app.post('/register', function(req, res){
+  db.collection.find({ 'username': username,'email':email }, function(err, user) {
+
+      if (err) {
+
+          console.log('Signup error');
+          return done(err);
+      }
+
+      //if user found.
+      if (user.length!=0) {
+        if(user[0].username){
+          console.log('Username already exists, username: ' + username);
+           }else{
+              console.log('EMAIL already exists, email: ' + email);
+           }
+          var err = new Error();
+          err.status = 310;
+          return done(err);
+
+        }
+    });
   db.collection('userdetails').save(req.body, function(err, result) {
   if (err) throw err;
   console.log('saved to database')
