8c9d18dd93f2931ca89443b73634cdad744650ee
AlexThomson9 <1507262@rgu.ac.uk>
2018-04-17 14:44:32 +0100

    ;;

---
+++ public/js/admin-check.js
@@ -0,0 +1,5 @@
+console.log(window.location.href);
+if(window.location.href == "http://flood-toyota-8080.codio.io/Admin"){
+
+  $('head').append('<link rel="stylesheet" href="admin.css" type="text/css" />');
+}
--- server.js
+++ server.js
@@ -41,8 +41,8 @@ app.get('/Contact', function(req, res) {
 app.get('/Admin', function(req, res) {
  res.render('pages/Admin');
 });
-app.get('/Mobile', function(req, res) {
- res.render('pages/Mobile');
+app.get('/mobile', function(req, res) {
+ res.render('pages/mobile');
 });
 app.get('/Countries', function(req, res) {
  db.collection('suggest').distinct('Country', function(err, result) {
