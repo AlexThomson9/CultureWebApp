7859c078af57ca8ec4f59a76a36f6c578b786298
AlexThomson9 <1507262@rgu.ac.uk>
2018-04-17 14:27:50 +0100

    gg missed a /

--- public/js/mobile_check.js
+++ public/js/mobile_check.js
@@ -1,6 +1,6 @@
 //If statement that occurs if the user is on a mobile device
 console.log(window.location.href);
-if(window.location.href == "http://flood-toyota-8080.codio.io"){
+if(window.location.href == "http://flood-toyota-8080.codio.io/"){
 
 
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
