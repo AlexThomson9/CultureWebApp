89661d435320ca3d3b5525f702a762747d6d56c9
AlexThomson9 <1507262@rgu.ac.uk>
2018-04-17 14:56:53 +0100

    test

--- public/js/admin-check.js
+++ public/js/admin-check.js
@@ -1,5 +1,5 @@
 console.log(window.location.href);
 if(window.location.href == "http://flood-toyota-8080.codio.io/Admin"){
 
-  $('head').append('<link rel="stylesheet" href="admin.css" type="text/css" />');
+  $('head').append('<link rel="stylesheet" href="/css/admin.css" type="text/css" />');
 }
