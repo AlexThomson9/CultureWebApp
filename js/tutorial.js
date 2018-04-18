23d203398b34d27f15f4024addf56ffe7a9a9537
Vinterdraken <mateo.rat01@gmail.com>
2018-04-18 14:53:26 +0100

    [tuto] first steps

--- js/tutorial.js
+++ js/tutorial.js
@@ -1,19 +1,21 @@
 var tutorial_step = 0;
 var popUp;
 
+var text_step_1 = "Let's go for a short Tutorial, to teach you how I work !";
+var text_step_2 = "Here's the Home page button, it will get where we are now.";
+var text_step_3 = "Here's the Login button, where you can Log to your account.";
+var text_step_4 = "Here's the Register button, it will allow to create an account.";
+var text_step_5 = "Here's the Contact button, it will allow to contact my devellopers for any questions or anything else.";
+var text_step_6 = "Here's the Suggest button, through it you will be allowed to improve my knowledge.";
+var text_step_7 = "You can select a country's informations by click on the marker in the country you want, try it!";
+
+var text_step_8 = "So there is the information panel, about the country you've selected.\nYou can choose between 4 rubrics, Customs, Gestures, Culture, Laws";
+var text_step_9 = "";
+var text_step_10 = "";
+
+
+
 $(function(){ 
-
-	var text_step_1 = "Let's go for a short Tutorial, to teach you how I work !";
-	var text_step_2 = "Here's the Home page button, it will get where we are now.";
-	var text_step_3 = "Here's the Login button, where you can Log to your account.";
-	var text_step_4 = "Here's the Register button, it will allow to create an account.";
-	var text_step_5 = "Here's the Contact button, it will allow to contact my devellopers for any questions or anything else.";
-	var text_step_6 = "Here's the Suggest button, through it you will be allowed to improve my knowledge.";
-	var text_step_7 = "You can select a country's informations by click on the marker in the country you want, try it!";
-
-	var text_step_8 = "So there is the information panel, about the country you've selected.\nYou can choose between 4 rubrics, Customs, Gestures, Culture, Laws";
-	var text_step_9 = "";
-	var text_step_10 = "";
 
 	if(tutorial_step == 0 || tutorial_step == null){
 		var defaultPopUp = createDefaultPopUp();
@@ -22,6 +24,7 @@ $(function(){
 
 		$('#default_sprite').onClick(function(){
 			tutorial_step++;
+			console.log("you clicked the sprite");
 		});
 	}
 
@@ -74,27 +77,23 @@ function createTutotialPopUp(text, tutorial_step){
 
 	var popUp = "<div class=\"div-tuto-step_" + tutorial_step + "\">";s
 
-		popUp += "<div class=\"tutorial-popUp-textAndButtons\">";
+		popUp += "<div class=\"tutorial-textAndButtons\">";
 
-			popUp += "<p id=\"tutorial-popUp-text\">" + text + "</p>";
-			popUp += "<button id=\"tutorial-popUp-gotItButton\">Got It</button>";
-			popUp += "<a id=\"tutorial-popUp-skipButton\" href=\"#\" >Skip tutorial</a>";
+			popUp += "<p id=\"tutorial-text\">" + text + "</p>";
+			popUp += "<button id=\"tutorial-gotItButton\">Got It</button>";
+			popUp += "<a id=\"tutorial-skipButton\" href=\"#\" >Skip tutorial</a>";
 
 		popUp += "</div>";
 
 		popUp += "<div class=\"sprite\">"
 
-			if(tutorial_step % 2 == 1 ){
+			if(tutorial_step == 1 ){
 				
-				popUp += "<img id=\"sprite_1\" src=\"images/sprite_1_talking.png\" height=\"385\" width=\"373\">";
-				popUp += "</div><div class=\"bubble\">"
-				popUp += "<img id=\"bubble_1\" scr=\"images/pop-up_bubble_1.png\">";
+				popUp += "<img id=\"sprite_1\" src=\"images/sprite_1_talking.png\" height=\"385\" width=\"373\"></div>";
 			}
 
 			else {
-				popUp += "<img class=\"sprite\" id=\"sprite_2\" src=\"images/sprite_2.png\" height=\"209\" width=\"200\">";
-				popUp += "</div><div class=\"bubble\">"
-				popUp += "<img class=\"bubble\" id=\"bubble_2\" scr=\"images/pop-up_bubble_2.png\">";
+
 			}
 
 		popUp += "</div>";
@@ -120,7 +119,7 @@ function createDefaultPopUp(){
 	
 	var defaultPopUp = "<img id=\"default_sprite\" src=\"images/sprite_1.png\" height=\"174\" width=\"171\">";
     
-	return defaultPopUp
+	return defaultPopUp;
 }
 
 function attachPopUpBy(name, type, popUp){
