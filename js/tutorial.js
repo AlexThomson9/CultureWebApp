b6c488ec0de87113154e369fc67e15c4542d0220
Vinterdraken <mateo.rat01@gmail.com>
2018-04-18 17:34:44 +0100

    [tuto] first steps

--- js/tutorial.js
+++ js/tutorial.js
@@ -1,58 +1,34 @@
+//GLOBAL VARIABLE//
+var text_step_1 = "Let's go for a short Tutorial, to teach you how I work !";
+var text_step_2 = "Here's the Home page button, it will get where we are now.";
+var text_step_3 = "Here's the Login button, where you can Log to your account.";
+var text_step_4 = "Here's the Register button, it will allow to create an account.";
+var text_step_5 = "Here's the Contact button, it will allow to contact my devellopers for any questions or anything else.";
+var text_step_6 = "Here's the Suggest button, through it you will be allowed to improve my knowledge.";
+var text_step_7 = "You can select a country's informations by click on the marker in the country you want, try it!";
+
+var text_step_8 = "So there is the information panel, about the country you've selected.\nYou can choose between 4 rubrics, Customs, Gestures, Culture, Laws";
+var text_step_9 = "";
+var text_step_10 = "";
+
+var tutorial_current_step = 0;
+var last_step = 0;
+var popUp;
 
 //FUNCTION WHICH IS CALLED AUTOMATICLY//
 $(function(){ 
-
-	var text_step_1 = "Let's go for a short Tutorial, to teach you how I work !";
-	var text_step_2 = "Here's the Home page button, it will get where we are now.";
-	var text_step_3 = "Here's the Login button, where you can Log to your account.";
-	var text_step_4 = "Here's the Register button, it will allow to create an account.";
-	var text_step_5 = "Here's the Contact button, it will allow to contact my devellopers for any questions or anything else.";
-	var text_step_6 = "Here's the Suggest button, through it you will be allowed to improve my knowledge.";
-	var text_step_7 = "You can select a country's informations by click on the marker in the country you want, try it!";
-
-	var text_step_8 = "So there is the information panel, about the country you've selected.\nYou can choose between 4 rubrics, Customs, Gestures, Culture, Laws";
-	var text_step_9 = "";
-	var text_step_10 = "";
-
-
-	var tutorial_current_step = 0;
-	var last_step = 0;
-	var popUp;
 
 	var defaultPopUp = createDefaultPopUp();
 	attachPopUpBy("tutorial", "id", defaultPopUp);
 
 	$('#default_sprite').click(function(){
 		tutorial_current_step++;
-		last_step = tutorial_current_step;
 		$(this).detach();
 
 		createPopUpBY(tutorial_current_step);
 		attachPopUpBy("tutorial", "id", popUp);
 
-		do{			
-
-			if(last_step != tutorial_current_step){
-
-				detachPopUpBY(last_step);
-
-				last_step = tutorial_current_step;
-
-				createPopUpBY(tutorial_current_step);
-				attachPopUpBy("tutorial", "id", popUp);
-			}
-
-			$('#sprite_' + tutorial_current_step).click(function(){
-				$('div-tuto-step_' + tutorial_current_step).detach();
-				tutorial_current_step++;
-			});
-
-			$('#tutorial-skipButton').click(function(){
-				$('div-tuto-step_' + tutorial_current_step).detach();
-				tutorial_current_step = 0;
-			});
-
-		}while(tutorial_current_step != 0 && tutorial_current_step < 10);
+		tutorial(tutorial_current_step);
 	});
 
 	//TitleBar-d3
@@ -113,6 +89,7 @@ function detachPopUpBY(tutorial_current_step){
 	popUp = $('div-tuto-step_' + tutorial_current_step).detach();
 }
 
+/*
 function updateCssOfTutorialDiv(tutorial_current_step){
 	switch(tutorial_current_step){
 		case 1:
@@ -147,6 +124,7 @@ function updateCssOfTutorialDiv(tutorial_current_step){
 			break;
 	}
 }
+*/
 
 function createPopUpBY(tutorial_current_step){
 
@@ -183,4 +161,44 @@ function createPopUpBY(tutorial_current_step){
 	}
 
 	return local_popUp;
+}
+
+function tutorial(tutorial_current_step){
+
+	if(tutorial_current_step == 0){
+		return;
+	}
+
+	if(last_step != tutorial_current_step){
+
+		if(last_step != 0){
+			detachPopUpBY(last_step);
+		}
+		
+		last_step = tutorial_current_step;
+
+		createPopUpBY(tutorial_current_step);
+		attachPopUpBy("tutorial", "id", popUp);
+
+		$('#tutorial-skipButton').click(function(){
+
+			$('div-tuto-step_' + tutorial_current_step).detach();
+			tutorial_current_step = 0;
+		});
+
+		$('#sprite_' + tutorial_current_step).click(function(){
+
+			$('div-tuto-step_' + tutorial_current_step).detach();
+			tutorial_current_step++;
+
+			if(tutorial_current_step < 10){
+				tutorial(tutorial_current_step);
+			}
+			else{
+				tutorial_current_step = 0;
+			}		
+		});
+	}
+
+	return;
 }
