2997a9d5b76266f96c46dd9bc19314c102666c18
OLEK WOZNIAK (1407272) <wozniok96@gmail.com>
2018-04-19 11:22:21 +0100

    fix

--- public/js/login.js
+++ public/js/login.js
@@ -21,6 +21,15 @@ $('#submitBtn').click(function(){
 
           if(data.length > 0){
             console.log(data);
+            var delayInMilliseconds = 2000; //2 second
+            //set the timeout to run the function
+            setTimeout(function()
+            {
+            //remove the submission successful as it only needs to be there for a little bit
+            $('.login-box').find("h5").remove();
+            window.location.replace("/Index");
+            //set the timeout to use the 2 second's defined previously
+          }, delayInMilliseconds);
             if(typeof(Storage) != "undefined"){
 
 
@@ -32,6 +41,7 @@ $('#submitBtn').click(function(){
             console.log(localStorage.getItem("User_Permission"));
             console.log(localStorage.getItem("username"));
                 }
+
             $('.login-box').find("h5").remove();
             $('.login-box').append("<h5>Logging in..<h5>");
 
@@ -39,15 +49,7 @@ $('#submitBtn').click(function(){
             $('.login-box').find("h5").remove();
             $('.login-box').append("<h5>User details entered incorrect, please try again!</h5>");
                 //set the var to 2 sectons
-                    var delayInMilliseconds = 2000; //2 second
-                    //set the timeout to run the function
-                    setTimeout(function()
-                    {
-                    //remove the submission successful as it only needs to be there for a little bit
-                    $('.login-box').find("h5").remove();
-                    window.location.replace("login.html");
-                    //set the timeout to use the 2 second's defined previously
-                  }, delayInMilliseconds);
+
                   form.reset();
           }
         }
