b3a975ea8a0527f8a02b16d3e302b2e91bc9891c
AlexThomson9 <1507262@rgu.ac.uk>
2018-04-19 09:40:06 +0100

    image loading

--- public/js/image.js
+++ public/js/image.js
@@ -6,16 +6,16 @@ my_image.src = '../images/background.jpg';
 //Define Image
 var my_image_2 = new Image();
 //Load culture image for the mobile modal
-my_image_2.src = '../images/culture.jpg';
+my_image_2.src = '../images/culture.jpeg';
 //Define Image
 var my_image_3 = new Image();
 //Load customs image for the mobile modal
-my_image_3.src = '../images/customs.jpg';
+my_image_3.src = '../images/customs.jpeg';
 //Define Image
 var my_image_4 = new Image();
 //Load gestures image for the mobile modal
-my_image_4.src = '../images/gestures.jpg';
+my_image_4.src = '../images/gestures.jpeg';
 //Define Image
 var my_image_5 = new Image();
 //Load law image for the mobile modal
-my_image_5.src = '../images/law.jpg';
+my_image_5.src = '../images/law.jpeg';
