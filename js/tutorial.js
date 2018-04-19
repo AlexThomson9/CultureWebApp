6f8042875c1a58ae454adf9319f7a8217009d26d
Vinterdraken <mateo.rat01@gmail.com>
2018-04-19 09:58:20 +0100

    [CSS] placing steps of tutorial

--- js/tutorial.js
+++ js/tutorial.js
@@ -1,13 +1,13 @@
 //GLOBAL VARIABLE//
 var text_step_1 = "Let's go for a short Tutorial, to teach you how I work !";
-var text_step_2 = "Here's the Home page button, it will get where we are now.";
-var text_step_3 = "Here's the Login button, where you can Log to your account.";
-var text_step_4 = "Here's the Register button, it will allow to create an account.";
-var text_step_5 = "Here's the Contact button, it will allow to contact my devellopers for any questions or anything else.";
-var text_step_6 = "Here's the Suggest button, through it you will be allowed to improve my knowledge.";
-var text_step_7 = "You can select a country's informations by click on the marker in the country you want, try it!";
-var text_step_8 = "So there is the information panel, about the country you've selected.\nYou can choose between 4 rubrics, Customs, Gestures, Culture, Laws";
-var text_step_9 = "Text 9";
+var text_step_2 = "Here youu can choose a continent, then the map will move on the part of the world you've selected.";
+var text_step_3 = "Here's the Home page button, it will get where we are now.";
+var text_step_4 = "Here's the Login button, where you can Log to your account.";
+var text_step_5 = "Here's the Register button, it will allow to create an account.";
+var text_step_6 = "Here's the Contact button, it will allow to contact my devellopers for any questions or anything else.";
+var text_step_7 = "Here's the Suggest button, through it you will be allowed to improve my knowledge.";
+var text_step_8 = "You can select a country's informations by click on the marker in the country you want, try it!";
+var text_step_9 = "So there is the information panel, about the country you've selected.\nYou can choose between 4 rubrics, Customs, Gestures, Culture, Laws";
 
 var tutorial_current_step;
 var last_step;
@@ -34,6 +34,9 @@ function createTutotialPopUp(text){
 
 	if(tutorial_current_step == 1){
 		popUp_temp += "<img id=\"sprite_" + tutorial_current_step + "\" src=\"images/sprite_1_talking.png\" height=\"385\" width=\"373\"></div>";
+	}
+	else if(tutorial_current_step == 2){
+		popUp_temp += "<img id=\"sprite_" + tutorial_current_step + "\" src=\"images/sprite_2_talking_2.png\" height=\"405\" width=\"307\"></div>";
 	}
 	else {
 		popUp_temp += "<img id=\"sprite_" + tutorial_current_step + "\" src=\"images/sprite_2_talking.png\" height=\"405\" width=\"307\"></div>";
