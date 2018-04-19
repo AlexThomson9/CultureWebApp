713c80222a91d22aaa21b15f0d8da9c83b244bfc
AlexThomson9 <1507262@rgu.ac.uk>
2018-04-19 12:39:47 +0100

    sda

--- public/js/UserCheck.js
+++ public/js/UserCheck.js
@@ -1,9 +1,9 @@
 
 //localStorage.setItem("user_id", user_id);
 //localStorage.setItem("User_Permission", User_Permission);
-if (localStorage.getItem("user_id") == NULL){
+if (localStorage.getItem("user_id") == ""){
   console.log("no one is logged in");
-}else if(localStorage.getItem("user_id") != NULL){
+}else if(localStorage.getItem("user_id") != ""){
   $('.LoggedIn').hide();
   $('Suggest').show();
   if(localStorage.getItem("User_Permission") == "Admin" ){
