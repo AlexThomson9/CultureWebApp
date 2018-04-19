fc6f964933b608eb1c467a65a6496dff26914013
ConnorForsyth <connorforsyth34@gmail.com>
2018-04-19 09:04:25 +0100

    JSON format for database

---
+++ public/js/culture.js
@@ -0,0 +1,80 @@
+{
+	"countries": [{
+			"name": "United Kingdom",
+			"culture": "The theatres have long been well supported with entertainment ranging from music to drama and to comedy. In the 19th Century, the Music Hall was the mainstay of entertainment offering all manner of acts from singing to acrobatics.",
+			"laws": "It is illegal to sing any profane or obscene song or ballad in any street. Dressing up as an Soldier for a fancy dress party could land you three months in jail, the law states it is an offence to impersonate the holder of a certificate of service or discharge.",
+			"customs": "Traditionally music and social gatherings have been the cement in isolated communities over the centuries with dancing and singing. The Scottish and Irish Ceilidh is a traditional social gathering involving Gaelic folk music and dancing either in a house or larger venue. The Welsh people are known for their singing voices and the Welsh Men’s Choir is renowned World Wide. Please avoid doing the following whilst you visit Britain: Do not rest your elbows on the table, this is seen as a rude gesture. Do not ask personal questions such as how much someone earns, who they voted for etc and do not speak too loudly or cut into a conversation.",
+			"gestures": "The two-fingered salute insult is used to express your distaste for a person or their actions. The middle finger is used to express displeasure at a person, and usually means you want them to leave you alone."
+		},
+		{
+			"name": "France",
+			"culture": "The French take a great deal of pride towards their country and government and typically offended by negative comments about their country. ",
+			"laws": "Drivers are legally required to carry a portable breathalyzer in their vehicle. You must buy a ticket for any live snails you take onto a high-speed train. You cannot take photos of police officers or their vehicles.",
+			"customs": "N/A",
+			"gestures": "The eyelid pull is when you place your finger in the center of your eyelid and pull the skin downward, is common in France, Italy, Spain and Greece. However, in France and Greece the signal is more of a warning, in order to highlight they are watching you, or keeping an eye on you. Whereas in Spain and Italy it is considered a friendlier warning that someone is clever or aware."
+		},
+		{
+			"name": "Spain",
+			"culture": "La Comida, the equivalent of lunch in english, is usually between 13:00 – 15:30. During this time of the day, expect to see smaller shops and restaurants closed.",
+			"laws": "Spanish law defines anyone under 18 to be a minor. Any unaccompanied minors that come to the attention of the Spanish authorities are judged to be vulnerable and may be taken into a minors centre until a parent or guardian can be found. In some parts of Spain it’s against the law to be in the street wearing only a bikini or swimming shorts/trunks. Being bare-chested has also been banned. Some local councils will impose fines if you’re caught wearing swimwear on the seafront promenade or the adjacent streets. Some local councils in Spain have banned the consumption of alcohol in the street and on-the-spot fines may be issued. There are strict controls on drinking and sexual activity in public places, including beaches. Spain is tolerant and socially progressive on attitudes towards the LGBT community. Madrid held the World Pride celebrations in 2017 and same-sex marriage has been legal in Spain since 2005. ",
+			"customs": "Please avoid doing the following whilst you visit Spain: Do not talk about religion. The Spanish have a strong sense of religious pride. Do not expect dinner in the evening to be any time before 9pm. Do not plan anything for a Tuesday 13th as this is seen as an unlucky date.",
+			"gestures": "Don’t pass the salt shaker from hand to hand. According to Spanish tradition, it brings bad luck."
+		},
+		{
+			"name": "Italy",
+			"culture": "Appearances matter in Italy, the way you dress can indicate your social status, your family's background, and your education level.",
+			"laws": "By law you must be able to show some form of identification at all times. In most cases a photocopy of the data page of your passport should suffice, but you may be asked to accompany the police to collect the original document, or to produce it within 12 hours. The police will normally ask for your full passport if you are stopped while driving. Illegal traders operate on the streets of all major Italian cities, particularly tourist cities like Florence, Venice and Rome. Don’t buy from illegal street traders. You could be stopped by the local police and fined. In some Italian towns and cities you may be fined for dropping litter and in some towns or cities it’s an offense to sit on monument steps or to eat and drink in the immediate vicinity of main churches, historic monuments and public buildings. It’s also an offence to enter or bathe in public fountains. Many major cities in Italy now impose a small tax on tourists. The tax is levied by hotels and is usually not included in any pre-paid arrangements or package deal. The rate of tax varies from city to city, and can depend on the star rating of the hotel. Hotels often ask for payment of this tax in cash. Make sure you get a receipt. ",
+			"customs": "If you are invited for a meal hosted by an Italian, remain standing until invited to sit down. You may be shown to a particular seat. Many Italians use calling cards in social situations. These are slightly larger than traditional business cards and include the person's name, address, title or academic honours, and their telephone number. ",
+			"gestures": "N/A"
+		},
+		{
+			"name": "Germany",
+			"culture": "N/A",
+			"laws": "It is an offence punishable with a fine and points on your licence to run out of petrol on a motorway. A pillow is classified as a 'passive weapon' and hitting someone with one can lead to charges of assault. ",
+			"customs": " ",
+			"gestures": "Putting your thumb between your index and middle fingers, is offensive as it symbolizes a sexual act, and is seen as being highly insulting. Forearm Jerk: A crude gesture, used in other countries too, the forearm jerk is not treated as a casual joke in Germany.  It is performed by making a fist with the right hand and bringing the arm up whilst simultaneously slapping the upper part of the right arm with the left hand.  It means the same as saying “get lost” or “go screw yourself”. "
+		},
+		{
+			"name": "Austria",
+			"culture": "N/A",
+			"laws": " ",
+			"customs": " ",
+			"gestures": " "
+		},
+		{
+			"name": "Poland",
+			"culture": "N/A",
+			"laws": " ",
+			"customs": " ",
+			"gestures": " "
+		},
+		{
+			"name": "Russia",
+			"culture": "N/A",
+			"laws": " ",
+			"customs": " ",
+			"gestures": " "
+		},
+		{
+			"name": "Turkey",
+			"culture": "N/A",
+			"laws": " ",
+			"customs": " ",
+			"gestures": " "
+		},
+		{
+			"name": "Greece",
+			"culture": "N/A",
+			"laws": " ",
+			"customs": " ",
+			"gestures": " "
+		},
+		{
+			"name": "India",
+			"culture": "N/A",
+			"laws": " ",
+			"customs": " ",
+			"gestures": " "
+		}
+	]
+}
