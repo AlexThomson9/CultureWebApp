9270bf0cd492c2d00d1f3ed303a05e29a82715a2
ConnorForsyth <connorforsyth34@gmail.com>
2018-04-19 11:34:17 +0100

    Removing js file

--- public/js/register.js
+++
@@ -1,5 +0,0 @@
-//On click function when the submit button is clicked
-$('#submitBtn').click(function () {
-  //When backend is being done, the validation for checking entry will be moved into
-  $(location).attr('href', 'test.html')
-});
