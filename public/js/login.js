8089f93e8eb72351c9ad2e24876f87b77d7e4f28
ConnorForsyth <connorforsyth34@gmail.com>
2018-04-20 12:06:49 +0100

    Nulling all over the world

--- public/js/login.js
+++ public/js/login.js
@@ -16,7 +16,7 @@ $('#submitBtn').click(function(){
   obj.username = user;
   obj.password = password;
 
-  if(obj.username == NULL || obj.password == NULL){
+  if(obj.username == "" || obj.password == ""){
     return;
   }
 
