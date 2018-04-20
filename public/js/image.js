3824017ee7f59a97cee8ae95c580b36405b692ea
AlexThomson9 <1507262@rgu.ac.uk>
2018-04-20 09:13:23 +0100

    ...

--- public/js/image.js
+++ public/js/image.js
@@ -1,2 +1,21 @@
+//This is to load the images upon visiting the index page, to reduce loading for the images
+//Define Image
 var my_image = new Image();
+//Load the background
 my_image.src = '../images/background.jpg';
+//Define Image
+var my_image_2 = new Image();
+//Load culture image for the mobile modal
+my_image_2.src = '../images/culture.jpeg';
+//Define Image
+var my_image_3 = new Image();
+//Load customs image for the mobile modal
+my_image_3.src = '../images/customs.jpeg';
+//Define Image
+var my_image_4 = new Image();
+//Load gestures image for the mobile modal
+my_image_4.src = '../images/gestures.jpeg';
+//Define Image
+var my_image_5 = new Image();
+//Load law image for the mobile modal
+my_image_5.src = '../images/law.jpeg';
