afaebc79e8eb556b46c533dc0635e23e77a6c6d7
ConnorForsyth <connorforsyth34@gmail.com>
2018-04-20 19:10:17 +0100

    Testing if database record is empty

--- public/js/countryModal.js
+++ public/js/countryModal.js
@@ -19,7 +19,12 @@ $('#customsClick, #gesturesClick, #cultureClick, #lawClick').click(function(even
 
 $('#customsClick').click(function(event){
   serverContent.style.display = "block";
+  if(testArray[0].customs == ""){
+    $('.serverContent').empty().append("We are sorry we do not have the information that you are looking for. If you would like to help us and fellow visitors, please login and provide us with new information by going to the Suggest page");
+  }
+  else{
   $('.serverContent').empty().append(testArray[0].customs);
+ }
 });
 
 $('#gesturesClick').click(function(event){
