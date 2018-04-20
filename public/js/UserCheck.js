483a0da15d98984b7f3062fd6b8f899265b72d8d
AlexThomson9 <1507262@rgu.ac.uk>
2018-04-20 08:11:18 +0100

    :;p

--- public/js/UserCheck.js
+++ public/js/UserCheck.js
@@ -4,6 +4,10 @@
 if (localStorage.getItem("user_id") == ""){
   console.log("no one is logged in");
 }else{
+  console.log("you logged in Successfully");
+  console.log(localStorage.getItem("user_id"));
+  console.log(localStorage.getItem("User_Permission"));
+  console.log(localStorage.getItem("username"));
   $('.LoggedIn').hide();
   $('Suggest').show();
   if(localStorage.getItem("User_Permission") == "Admin" ){
