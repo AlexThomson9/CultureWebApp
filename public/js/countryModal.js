fca53bda97a31dd62bf986d98b9814bb4963061c
ConnorForsyth <connorforsyth34@gmail.com>
2018-04-20 19:11:47 +0100

    Logging the returned data

--- public/js/countryModal.js
+++ public/js/countryModal.js
@@ -19,6 +19,7 @@ $('#customsClick, #gesturesClick, #cultureClick, #lawClick').click(function(even
 
 $('#customsClick').click(function(event){
   serverContent.style.display = "block";
+  console.log(testArray[0].customs);
   if(testArray[0].customs == ""){
     $('.serverContent').empty().append("We are sorry we do not have the information that you are looking for. If you would like to help us and fellow visitors, please login and provide us with new information by going to the Suggest page");
   }
