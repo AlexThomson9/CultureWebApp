35a6a11d1060b8173dcf3c8b47ddfff18b3fc006
ConnorForsyth <connorforsyth34@gmail.com>
2018-04-20 19:24:10 +0100

    Wahoo x5

--- public/js/countryModal.js
+++ public/js/countryModal.js
@@ -19,7 +19,7 @@ $('#customsClick, #gesturesClick, #cultureClick, #lawClick').click(function(even
 
 $('#customsClick').click(function(event){
   serverContent.style.display = "block";
-  if(typeof testArray[0].customs == undefined){
+  if(typeof testArray[0].customs === "undefined"){
     $('.serverContent').empty().append("We are sorry we do not have the information that you are looking for. If you would like to help us and fellow visitors, please login and provide us with new information by going to the Suggest page");
   }
   else{
