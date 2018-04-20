ebcd172d13b60f9080d40c7a37fa7e14fd048222
ConnorForsyth <connorforsyth34@gmail.com>
2018-04-20 19:52:27 +0100

    Test

--- public/js/countryModal.js
+++ public/js/countryModal.js
@@ -24,6 +24,7 @@ $('#customsClick').click(function(event){
     $('.serverContent').empty().append("We are sorry we do not have the information that you are looking for. If you would like to help us and fellow visitors, please login and provide us with new information by going to the Suggest page");
   }
   else{
+    console.log("I got here wahoo");
     $('.serverContent').empty().append(testArray[0].customs);
   }
 //  if(testArray[0].customs == undefined){
