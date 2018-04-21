6f75c7509cfb783a9015e43a43ae2f6a17f96a09
ConnorForsyth <connorforsyth34@gmail.com>
2018-04-21 13:27:40 +0100

    :(((

--- public/js/countryModal.js
+++ public/js/countryModal.js
@@ -20,7 +20,7 @@ $('#customsClick, #gesturesClick, #cultureClick, #lawClick').click(function(even
 $('#customsClick').click(function(event){
   serverContent.style.display = "block";
   console.log(typeof testArray[0].customs === "undefined");
-  if(typeof testArray[0].customs != "undefined"){
+  if(typeof testArray[0].customs != undefined){
       $('.serverContent').empty().append(testArray[0].customs);
       console.log("hi");
   }
