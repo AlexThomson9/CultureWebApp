15bb078583389629a58b30f936a63378ba36c55c
AlexThomson9 <1507262@rgu.ac.uk>
2018-04-23 10:59:08 +0100

    gg

--- public/js/mobile_check.js
+++ public/js/mobile_check.js
@@ -1,18 +1,18 @@
 //Log what the URl is for the site to get what page its on
 console.log(window.location.href);
 
-var url = window.location.href
+var url = window.location.href;
 //If the window url (page) is equal to the home page or index then continue
-if(url.indexOf("Admin", "Login", "Register", "Suggest") ==){
+if(url.indexOf("Admin", "Login", "Register", "Suggest") == -1){
   console.log("plswork");
 
 //if the device is mobile then continue
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
   //Redirects the user to the mobile version of the website
-  $(location).attr('href', 'Mobile-index')
+  $(location).attr('href', 'Mobile-index');
 }
 }else{
 
 
-  console.log(":( i cri )");
+  console.log(" i cri ");
 }
