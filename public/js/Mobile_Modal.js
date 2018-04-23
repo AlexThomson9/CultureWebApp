48885cbe09a4f94e417f78d7c20e1304596535c8
ConnorForsyth <connorforsyth34@gmail.com>
2018-04-23 12:29:52 +0100

    ...

--- public/js/Mobile_Modal.js
+++ public/js/Mobile_Modal.js
@@ -115,8 +115,7 @@ $(".leaflet-marker-icon.leaflet-zoom-animated.leaflet-interactive").click(functi
        $('.serverContent').empty().append("We are sorry we do not have the information that you are looking for. If you would like to help us and fellow visitors, please login and provide us with new information by going to the Suggest page");
      }
      else{
-       $('.serverContent').empty().append(testArray[0].customs);
-       returnToPopupMenu.style.display = "block";
+       $('.serverContent').empty().append(testArray[0].customs);    
      }
    }
   });
