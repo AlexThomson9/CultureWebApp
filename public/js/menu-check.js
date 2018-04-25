40e3aca62a908916af831347982f0bae135461fe
AlexThomson9 <1507262@rgu.ac.uk>
2018-04-25 09:40:30 +0100

    gg

--- public/js/menu-check.js
+++ public/js/menu-check.js
@@ -1,9 +1,8 @@
 //Log url
 console.log(window.location.href);
 var url = window.location.href;
-//If you are on the admin page then
-if(!url.endsWith("/") == true){
-  //add the admin css to that pages head only otherise the others will break
-
+//If you are on the index page then
+if(url.endsWith("/") == false){
+  //hide the contitent dropdown 
 $('#Continent').hide();
 }
